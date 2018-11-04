0eb97618dd908aba15747f0ce8231127b69e607d
Mary Jack <maryjackinthebox@gmail.com>
2018-11-04 22:56:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /* Your code goes here */
+var customerName = 'bob';
+ 
+ function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+ }
+ 
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Avia';
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'Franklin';
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'Kanye';
+  let favoriteCustomer = 'Kim';
+}
